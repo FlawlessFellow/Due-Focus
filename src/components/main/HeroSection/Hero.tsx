b7d6831ff00ba797f282fd5ad66bef0b3be2df5c
FlawlessFellow <flawlesselish@gmail.com>
2024-04-10 00:37:40 +0200

    Added the ability to change the text, which is called using a prop in the button, when the screen width is less than 1165px. Using useState & useEffect

--- src/components/main/HeroSection/Hero.tsx
+++ src/components/main/HeroSection/Hero.tsx
@@ -1,9 +1,35 @@
 import './style.css';
 import heroImg from './../../../assets/images/hero-right-block.png';
 import BlueBtn from '../../BlueBtn/BlueBtn';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Hero = () => {
+    // Set button text based on window width
+    const [buttonText, setButtonText] = useState('Sign Up for Free');
+
+    useEffect(() => {
+        // Handle window resize
+        const handleResize = () => {
+            const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+            if (window.innerWidth - scrollbarWidth < 1165) {
+                setButtonText('Get Started');
+            } else {
+                setButtonText('Sign Up for Free');
+            }
+        };
+
+        // Set initial state based on current window width
+        handleResize();
+
+        // Add event listener for window resize
+        window.addEventListener('resize', handleResize);
+
+        // Clean up event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <section id="hero" className="hero">
             <div className="container">
@@ -12,11 +38,7 @@ const Hero = () => {
                         <h1 className="hero__left-title">Time Tracking & Productivity Chrome App You'll Actually Use</h1>
                         <p className="hero__left-text">Available on Google Web Store</p>
                         <div className="hero__left-btn">
-                            <BlueBtn
-                                text={'Sign Up for Free'}
-                                classNames={'hero__blue-btn hvr-float-shadow '}
-                                onClick={() => null}
-                            />
+                            <BlueBtn text={buttonText} classNames={'hero__blue-btn hvr-float-shadow '} onClick={() => null} />
                         </div>
                     </div>
                     <div className="hero__right-block">
