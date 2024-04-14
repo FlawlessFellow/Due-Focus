832b39681f73c586e4c20f83eade69aacee123d8
FlawlessFellow <flawlesselish@gmail.com>
2024-04-14 22:03:10 +0200

    Completed the width adaptation for the Security section entirely, and made some minor changes within this section

--- src/components/main/DataSecuritySection/DataSecurity.tsx
+++ src/components/main/DataSecuritySection/DataSecurity.tsx
@@ -1,37 +1,24 @@
 import React from 'react';
 import './style.css';
-import SecuritySectionImg from './../../../assets/images/security-section.png';
+import SecuritySectionImg from './../../../assets/images/privacy.svg';
 
 const DataSecurity = () => {
     return (
         <section className="security__section">
-            <div className="container">
-                <div className="security__wrapper">
-                    <div className="security__title-wrapper">
-                        <h2 className="security__title">High Privacy and Security</h2>
+            <div className="security__wrapper">
+                <h2 className="security__title">High Privacy and Security</h2>
+                <div className="security__items-wrapper">
+                    <img src={SecuritySectionImg} alt="Data security logo" className="security__section-img" />
+                    <div className="right__item-info">
+                        <p className="right__item-text">
+                            Conformity of the company to the requirements of GDPR guarantees the proper level of the services
+                            offered to clients.
+                        </p>
+                        <div className="right__item-border"></div>
+                        <p className="right__item-text2">DATA PRIVACY WITH DROPBOX AND GOOGLE DRIVE</p>
+                        <p className="right__item-text3">Your data is always under protection</p>
                     </div>
-                    <div className="security__content-items">
-                        <div className="security__items-wrapper">
-                            <div className="left__item-img">
-                                <img src={SecuritySectionImg} alt="Data security logo" className="security__section-img" />
-                            </div>
-                            <div className="right__item-info">
-                                <div>
-                                    <p className="right__item-text">
-                                        Conformity of the company to the requirements of GDPR guarantees the proper level of the
-                                        services offered to clients.
-                                    </p>
-                                </div>
-                                <div className="right__item-border"></div>
-                                <div>
-                                    <p className="right__item-text2">DATA PRIVACY WITH DROPBOX AND GOOGLE DRIVE</p>
-                                </div>
-                                <div>
-                                    <p className="right__item-text3">Your data is always under protection</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <div className="security__arrow-block"></div>
                 </div>
             </div>
         </section>
