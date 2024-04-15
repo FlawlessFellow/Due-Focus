3935242f918ce093daec40b024f71c54ec4a929e
FlawlessFellow <flawlesselish@gmail.com>
2024-04-15 19:32:34 +0200

    Completed the width adaptation for the Footer section and made minor adjustments in this section. Also, I replaced regular <a> tags with <Link /> from Gatsby, and added :hover styles for these links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -14,18 +14,18 @@ const Footer = () => {
                 <div className="footer__wrapper">
                     <div className="footer__content">
                         <div className="footer__first-block">
-                            <Link to="#!" target={'_blank'}>
+                            <Link to="#!" target={'_blank'} className="footer__link">
                                 <img src={FooterLinkLogo} alt="footer link logo" />
                             </Link>
                             <p className="first__block-text">Track work hours with a single button</p>
                             <div className="first__block-imagesWrapper">
                                 <div className="first__block-images">
-                                    <Link to="#!" target={'_blank'}>
+                                    <Link to="#!" target={'_blank'} className="footer__link">
                                         <img src={FooterFacebookImg} alt="" />
                                     </Link>
                                 </div>
                                 <div className="first__block-images">
-                                    <Link to="#!" target={'_blank'}>
+                                    <Link to="#!" target={'_blank'} className="footer__link">
                                         <img src={FooterTwitterImg} alt="" />
                                     </Link>
                                 </div>
@@ -33,47 +33,83 @@ const Footer = () => {
                         </div>
                         <div className="footer__second-block">
                             <p className="footer__list-subtitleLink">
-                                <a href="">Diligences family</a>
+                                <Link to="#!" target={'_blank'} className="footer__link">
+                                    Diligences family
+                                </Link>
                             </p>
                             <p className="footer__list-text">
-                                <strong>Duefocus</strong>. Time tracking
+                                <Link to="#!" target={'_blank'} className="footer__link">
+                                    <strong>Duefocus</strong>. Time tracking
+                                </Link>
                             </p>
                             <p className="footer__list-text">
-                                <strong>Duecode</strong>. Engineering metrics
+                                <Link to="#!" target={'_blank'} className="footer__link">
+                                    <strong>Duecode</strong>. Engineering metrics
+                                </Link>
                             </p>
                             <p className="footer__list-text">
-                                <strong>Duerank</strong>. Global developers leaderboard
+                                <Link to="#!" target={'_blank'} className="footer__link">
+                                    <strong>Duerank</strong>. Global developers leaderboard
+                                </Link>
                             </p>
                             <p className="footer__list-text">
-                                <strong>Duelearn</strong>. Writing better code
+                                <Link to="#!" target={'_blank'} className="footer__link">
+                                    <strong>Duelearn</strong>. Writing better code
+                                </Link>
                             </p>
                             <p className="footer__list-lastText">
-                                <strong>Duetask</strong>. Set realistic estimates
+                                <Link to="#!" target={'_blank'} className="footer__link">
+                                    <strong>Duetask</strong>. Set realistic estimates
+                                </Link>
                             </p>
                         </div>
                         <div className="footer__third-block">
-                            <p className="footer__list-subtitleLink">
-                                <a href="">About</a>
+                            <p className="footer__list-subtitleLink">About</p>
+                            <p className="footer__list-text">
+                                <Link to="#!" target={'_blank'} className="footer__link">
+                                    Terms of Use
+                                </Link>
                             </p>
-                            <p className="footer__list-text">Terms of Use</p>
-                            <p className="footer__list-text">Privacy Policy</p>
-                            <p className="footer__list-lastText">Cookie Policy</p>
+                            <p className="footer__list-text">
+                                <Link to="#!" target={'_blank'} className="footer__link">
+                                    Privacy Policy
+                                </Link>
+                            </p>
+                            <p className="footer__list-lastText">
+                                <Link to="#!" target={'_blank'} className="footer__link">
+                                    Cookie Policy
+                                </Link>
+                            </p>
                         </div>
                         <div className="footer__fourth-block">
-                            <p className="footer__list-subtitleLink">
-                                <a href="">Contact us</a>
-                            </p>
+                            <p className="footer__list-subtitleLink">Contact us</p>
                             <p className="footer__list-text">297 NV-207 Stateline, NV 89449 USA</p>
                             <p className="footer__list-text">+1 310-388-9334</p>
-                            <p className="footer__list-lastText">support@diligences.com</p>
+                            <p className="footer__list-lastText">
+                                <Link to="#!" target={'_blank'} className="footer__link">
+                                    support@diligences.com
+                                </Link>
+                            </p>
+                            <div className="fourth__block-imagesWrapper">
+                                <div className="first__block-images">
+                                    <Link to="#!" target={'_blank'} className="footer__link">
+                                        <img src={FooterFacebookImg} alt="" />
+                                    </Link>
+                                </div>
+                                <div className="first__block-images">
+                                    <Link to="#!" target={'_blank'} className="footer__link">
+                                        <img src={FooterTwitterImg} alt="" />
+                                    </Link>
+                                </div>
+                            </div>
                             <div className="footer__lastBlock-images">
                                 <div className="lastBlock__first-img">
-                                    <Link to="#!" target={'_blank'}>
+                                    <Link to="#!" target={'_blank'} className="footer__link">
                                         <img src={LastBlockFirstImg} alt="footer logo" />
                                     </Link>
                                 </div>
                                 <div className="lastBlock__second-img">
-                                    <Link to="#!" target={'_blank'}>
+                                    <Link to="#!" target={'_blank'} className="footer__link">
                                         <img src={LastBlockSecondImg} alt="footer logo" />
                                     </Link>
                                 </div>
