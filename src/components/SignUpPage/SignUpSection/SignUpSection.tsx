f75d515d64de6ddd5b5b8e2fd2dad70a4b358211
FlawlessFellow <flawlesselish@gmail.com>
2024-04-24 15:44:44 +0200

    Created SignUpPage section

---
+++ src/components/SignUpPage/SignUpSection/SignUpSection.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+const SignUpSection = () => {
+    return (
+        <div className="sign-up">
+            <section className="sign__up-section">
+                <div className="sign__up-wrapper"></div>
+            </section>
+        </div>
+    );
+};
+
+export default SignUpSection;
